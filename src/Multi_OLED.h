96904eab9c5b9c4420a4e1a8146371c259c81354
Larry Bank <bitbank@pobox.com>
2019-01-10 16:38:57 -0500

    Fixed prototype names from bus to display terminology

--- src/Multi_OLED.h
+++ src/Multi_OLED.h
@@ -11,34 +11,34 @@ enum {
 };
 
 //
-// Initializes the OLED controller(s) into "page mode"
+// Initializes all OLED controller(s) into "page mode"
 //
-void Multi_OLEDInit(uint8_t *iBus, uint8_t *iAddr, uint8_t *iType, uint8_t *bFlip, uint8_t *bInvert, uint8_t iBusCount);
+void Multi_OLEDInit(uint8_t *iBus, uint8_t *iAddr, uint8_t *iType, uint8_t *bFlip, uint8_t *bInvert, uint8_t iCount);
 //
 // Sets the brightness (0=off, 255=brightest)
 //
-void Multi_OLEDSetContrast(uint8_t iBus, unsigned char ucContrast);
+void Multi_OLEDSetContrast(uint8_t iDisplay, unsigned char ucContrast);
 //
 // Load a 128x64 1-bpp Windows bitmap
 // Pass the pointer to the beginning of the BMP file
 // First pass version assumes a full screen bitmap
 //
-int Multi_OLEDLoadBMP(uint8_t iBus, byte *pBMP);
+int Multi_OLEDLoadBMP(uint8_t iDisplay, byte *pBMP);
 //
-// Power up/down the display
+// Power up/down a display
 // useful for low power situations
 //
-void Multi_OLEDPower(uint8_t iBus, byte bOn);
+void Multi_OLEDPower(uint8_t iDisplay, byte bOn);
 //
 // Draw a string of normal (8x8), small (6x8) or large (16x32) characters
 // At the given col+row
 //
-int Multi_OLEDWriteString(uint8_t iBus, int x, int y, char *szMsg, int iSize, int bInvert);
+int Multi_OLEDWriteString(uint8_t iDisplay, int x, int y, char *szMsg, int iSize, int bInvert);
 //
 // Fill the frame buffer with a byte pattern
 // e.g. all off (0x00) or all on (0xff)
 //
-void Multi_OLEDFill(uint8_t iBus, unsigned char ucData);
+void Multi_OLEDFill(uint8_t iDisplay, unsigned char ucData);
 //
 // Set (or clear) an individual pixel
 // The local copy of the frame buffer is used to avoid
@@ -47,5 +47,5 @@ void Multi_OLEDFill(uint8_t iBus, unsigned char ucData);
 // This function needs the USE_BACKBUFFER macro to be defined
 // otherwise, new pixels will erase old pixels within the same byte
 //
-int Multi_OLEDSetPixel(uint8_t iBus, int x, int y, unsigned char ucColor);
+int Multi_OLEDSetPixel(uint8_t iDisplay, int x, int y, unsigned char ucColor);
 
